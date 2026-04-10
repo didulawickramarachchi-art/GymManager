85567fa3136d0737dc80d925dd943c124a6e8361
DNWICK001 <didulanadeeja43@gmail.com>
2026-04-10 11:48:08 +0530

    Add login UI

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Gym Manager", style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+            SizedBox(height: 30),
+
+            TextField(decoration: InputDecoration(labelText: "Email")),
+            TextField(decoration: InputDecoration(labelText: "Password"), obscureText: true),
+
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                
+              },
+              child: Text("Login"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
